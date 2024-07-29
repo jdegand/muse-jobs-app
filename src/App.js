c55b8aa780bf8fd6a52581c226e46572bd775efc
J. Degand <70610011+jdegand@users.noreply.github.com>
2024-07-29 19:39:42 +0000

    fix: cleanup & format app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import Job from './components/Job';
 
 function App() {
@@ -12,22 +12,18 @@ function App() {
   const [companySearch, setCompanySearch] = useState('');
   const [locationSearch, setLocationSearch] = useState('');
 
-  //const [category, setCategory] = useState('');
-  //const [show, setShow] = useState(false)
-  //const [searchData, setSearchData] = useState('');
-
   const handleChange = (e) => {
     setCompanySearch(e.target.value)
-  } 
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    
+
     const found = acceptedCompanies.find(element => {
       return element.toLowerCase() === companySearch.toLowerCase()
     })
 
-    if(!found) {
+    if (!found) {
       alert(`${companySearch} doesn't exist or has no jobs listed.`)
     }
     const titleCase = companySearch.split(' ').map(i => i[0].toUpperCase() + i.slice(1)).join(' ')
@@ -35,7 +31,6 @@ function App() {
     setCompany(updated);
   }
 
-  // Array.from(document.querySelectorAll(".active-result")).map(x => x.innerText)
   const acceptedCompanies = [
     "Frontpoint", "Spryker", "Persado", "Klaviyo", "Sedera", "Betterment", "Northrop Grumman", "Lever", "4Degrees",
     "Billtrust", "Validity", "MoneyLion", "Liberty Mutual Insurance", "HP", "VMware", "TrackMaven", "Coursera",
@@ -226,7 +221,7 @@ function App() {
   //const acceptedCategories = ['Accounting','Accounting and Finance','Account Management','Account Management/Customer Success', 'Administration and Office', 'Advertising and Marketing','Animal Care','Arts', 'Business Operations','Cleaning and Facilities','Computer and IT', 'Construction', 'Corporate','Customer Service','Data and Analytics','Data Science','Design','Design and UX','Editor','Education', 'Energy Generation and Mining','Entertainment and Travel Services','Farming and Outdoors','Food and Hospitality Services','Healthcare','HR','Human Resources and Recruitment','Installation, Maintenance, and Repairs','IT','Law','Legal Services','Management', 'Manufacturing and Warehouse','Marketing','Mechanic','Media, PR, and Communications','Mental Health','Nurses','Office Administration','Personal Care and Services','Physical Assistant','Product','Product Management','Project Management','Protective Services','Public Relations','Real Estate','Recruiting','Retail','Sales','Science and Engineering','Social Services','Software Engineer','Software Engineering','Sports, Fitness, and Recreation','Transportation and Logistics','Unknown','UX','Videography','Writer','Writing and Editing']
 
   // level and location can be null
-  const fetchJobs = async(page, location, level) => {
+  const fetchJobs = async (page, location, level) => {
     const response = await fetch(`https://www.themuse.com/api/public/jobs?page=${page}&location=${location}&level=${level}`);
     if (!response.ok) {
       const message = `An error has occured: ${response.status}`;
@@ -237,7 +232,7 @@ function App() {
     return jobs;
   }
 
-  const fetchJobsByCompany = async(page, company) => { // location, level, &location=${location}&level=${level}
+  const fetchJobsByCompany = async (page, company) => { // location, level, &location=${location}&level=${level}
     const response = await fetch(`https://www.themuse.com/api/public/jobs?page=${page}&company=${company}`);
     if (!response.ok) {
       const message = `An error has occured: ${response.status}`;
@@ -248,7 +243,7 @@ function App() {
     return jobs;
   }
 
-  const fetchJobsOnFirstLoad = async() => {
+  const fetchJobsOnFirstLoad = async () => {
     const response = await fetch(`https://www.themuse.com/api/public/jobs?page=1`);
     if (!response.ok) {
       const message = `An error has occured: ${response.status}`;
@@ -283,21 +278,20 @@ function App() {
   const handleLocationSubmit = (e) => {
     e.preventDefault()
     // check if input has any value
-    if(e.target.previousElementSibling.value){
-      //setLocation(locationSearch.replace(' ', '%20'))
-      setLocation(encodeURI(locationSearch)) 
+    if (e.target.previousElementSibling.value) {
+      setLocation(encodeURI(locationSearch))
     }
   }
-  
-  useEffect(()=> {
+
+  useEffect(() => {
     fetchJobs(page, location, level)
   }, [page, location, level])
 
-  useEffect(()=> {
+  useEffect(() => {
     fetchJobsByCompany(page, company)
   }, [page, company])
 
-  useEffect(()=> {
+  useEffect(() => {
     fetchJobsOnFirstLoad()
   }, [])
 
@@ -305,15 +299,15 @@ function App() {
     <main className="App">
       <div className="logo-wrapper">
         <svg viewBox="-696 488.7 526.6 104.3" width="8em" height="4em" aria-labelledby="logo-title" aria-describedby="logo-desc">
-        <title id="logo-title">The Muse Logo</title>
-        <desc id="logo-desc">A logo with "the" in white text and "muse" in light blue text.</desc>
-        <path d="M-686.5 532.2h-8.5c-.6 0-1-.4-1-1v-12.6c0-.6.4-1 1-1h8c.6 0 1-.4 1-1v-18.1c0-.6.4-1 1-1h15.9c.6 0 1 .4 1 1v19.1h10.7c.6 0 1 .4 1 1v12.6c0 .6-.4 1-1 1h-9.7c-.6 0-1 .4-1 1v28.7c0 12.1 9.5 13.9 14.6 13.9.9 0 1.7-.1 2.2-.1.6-.1 1.1.4 1.1 1v14c0 .5-.4.9-.9 1-1 .1-2.7.3-4.8.3-10.3 0-30.6-3-30.6-27.7v-32.1zm154.4-16.4c21.1 0 32.6 15.5 32.6 34.9 0 2.2-.4 6.8-.4 6.8h-50.8c1.4 13 11 19.8 22.1 19.8 9.6 0 17.3-5.4 19.8-7.5.5-.4 1.2-.3 1.5.3l6.6 11c.2.4.2.9-.2 1.3-2.6 2.3-13.1 10.6-29 10.6-24.4 0-39.6-17.6-39.6-38.6 0-22.7 15.3-38.6 37.4-38.6zm13.8 29.1c-.3-8.8-6.4-14.9-13.9-14.9-9.4 0-16 5.6-18 14.9h31.9z" fill="#FFF"></path>
-        <path d="M-255 533.3v-.8c0-5.3-7.9-7.4-12.6-7.4-10.2 0-16.2 3.5-16.2 10.6 0 16.4 40.9 12.6 40.9 36.9 0 12.2-11 20.4-25.6 20.4-16.4 0-25.4-9.5-27.5-12.1-.3-.4-.3-.9 0-1.3l4.3-5.2c.4-.5 1.1-.5 1.5 0 2.3 2.6 9.9 10.1 21.8 10.1 8.2 0 15.4-3.9 15.4-11.6 0-16-40.9-13.1-40.9-36.6 0-13.2 11.3-19.7 26-19.7 7.4 0 22.2 2.9 22.2 13.2v1.8c0 .5-.4.9-.9 1l-8.4.7zm55-16.8c20.1 0 30.6 15.5 30.6 33.6 0 1.7-.3 4.9-.3 4.9h-56c.3 18.4 13.2 29 28.3 29 10.9 0 18.6-6 21.1-8.2.5-.4 1.2-.3 1.5.2l3.7 5.7c.3.4.2 1-.2 1.3-2.5 2.2-12.3 9.9-26.5 9.9-21.6 0-38.2-15.7-38.2-38.2 0-23.8 16.4-38.2 36-38.2zm20.4 30.8c-.6-15.1-9.9-22.5-20.6-22.5-12.1 0-22.7 7.9-25.1 22.5h45.7z" fill="#53B6D0"></path>
-        <path d="M-644.8 507.5v-17.8c0-.6.4-1 1-1h7.6c6.8 0 9.8 3 9.8 9.7v25.8c0 3.6-.3 6.2-.3 6.2h.3c3.5-7.1 12.6-14.6 25.3-14.6 16.5 0 25.7 8.5 25.7 28v47.3h-8.6c-6.8 0-9.7-2.9-9.7-9.8v-33.7c0-9-2.5-15-11.7-15-9.7 0-17.2 6.4-19.9 15.3-.9 2.9-1.2 6.1-1.2 9.5v32.8c0 .6-.4 1-1 1h-16.3c-.6 0-1-.4-1-1v-82.7" fill="#FFF"></path>
-        <path d="M-368.8 530v-10.8c0-.6.4-1 1-1h.7c6.2 0 8.3 2.6 8.3 8.6V562c0 11.5 1.9 21.7 16 21.7 16.5 0 26.9-14.7 26.9-30.5v-33.9c0-.6.4-1 1-1h7.9c.6 0 1 .4 1 1v71.9h-.8c-6.1 0-8.9-2.6-8.9-8.2v-7.3h-.3c-2.9 7.1-12.2 17.2-28.1 17.2-17.3 0-24.5-9.3-24.5-28.3V530h-.2" fill="#53B6D0"></path>
-        <g fill="#53B6D0"><path d="M-489.3 532.3v58.4c0 .6.4 1 1 1h7.9c.6 0 1-.4 1-1v-33.4c0-3.4.3-6.7 1.2-9.7 3.1-11.1 11.9-21.4 23.8-21.4 13.8 0 15.4 10.7 15.4 21.7v43c0 .6.4 1 1 1h7.9c.6 0 1-.4 1-1v-33.6c0-4 .3-7.4 1.3-10.6 3-10.7 12.1-20.6 23.1-20.6 13.4 0 15.8 9.8 15.8 21.7v35.1c0 6.3-.3 8.9 5.9 8.9h3.1c.6 0 1-.4 1-1v-45.5c0-18-6.6-28.3-24.3-28.3-12.3 0-23 8.4-26.9 18.3h-.3c-2.2-12.1-9.2-18.3-22.7-18.3-11.3 0-22.8 8.5-26.4 18.4h-.3s.3-2.3.3-5.2v-3.3c0-5.6.2-8.2-5.9-8.2h-4l.1 13.6z"></path>
-          <path d="M-380.1 591.8c-6.2 0-8.9-2.6-8.9-8.9v-35.1c0-11.9-2.5-21.7-15.8-21.7-11 0-20.1 9.9-23.1 20.6-1 3.2-1.3 6.6-1.3 10.6v34.5h-9.9v-44c0-11-1.6-21.7-15.4-21.7-11.9 0-20.7 10.3-23.8 21.4-.9 3-1.2 6.4-1.2 9.7v34.5h-9.9v-61.2c0-2-1.2-3.2-3.2-3.2h-6.1v-8.6h10.2c6.1 0 8.9 2.6 8.9 8.2v3.3c0 2.9-.3 5.2-.3 5.2h.3c3.6-9.9 15.1-18.4 26.4-18.4 13.5 0 20.4 6.2 22.7 18.3h.3c3.9-9.9 14.5-18.3 26.9-18.3 17.7 0 24.3 10.3 24.3 28.3V580"></path>
-        </g>
+          <title id="logo-title">The Muse Logo</title>
+          <desc id="logo-desc">A logo with "the" in white text and "muse" in light blue text.</desc>
+          <path d="M-686.5 532.2h-8.5c-.6 0-1-.4-1-1v-12.6c0-.6.4-1 1-1h8c.6 0 1-.4 1-1v-18.1c0-.6.4-1 1-1h15.9c.6 0 1 .4 1 1v19.1h10.7c.6 0 1 .4 1 1v12.6c0 .6-.4 1-1 1h-9.7c-.6 0-1 .4-1 1v28.7c0 12.1 9.5 13.9 14.6 13.9.9 0 1.7-.1 2.2-.1.6-.1 1.1.4 1.1 1v14c0 .5-.4.9-.9 1-1 .1-2.7.3-4.8.3-10.3 0-30.6-3-30.6-27.7v-32.1zm154.4-16.4c21.1 0 32.6 15.5 32.6 34.9 0 2.2-.4 6.8-.4 6.8h-50.8c1.4 13 11 19.8 22.1 19.8 9.6 0 17.3-5.4 19.8-7.5.5-.4 1.2-.3 1.5.3l6.6 11c.2.4.2.9-.2 1.3-2.6 2.3-13.1 10.6-29 10.6-24.4 0-39.6-17.6-39.6-38.6 0-22.7 15.3-38.6 37.4-38.6zm13.8 29.1c-.3-8.8-6.4-14.9-13.9-14.9-9.4 0-16 5.6-18 14.9h31.9z" fill="#FFF"></path>
+          <path d="M-255 533.3v-.8c0-5.3-7.9-7.4-12.6-7.4-10.2 0-16.2 3.5-16.2 10.6 0 16.4 40.9 12.6 40.9 36.9 0 12.2-11 20.4-25.6 20.4-16.4 0-25.4-9.5-27.5-12.1-.3-.4-.3-.9 0-1.3l4.3-5.2c.4-.5 1.1-.5 1.5 0 2.3 2.6 9.9 10.1 21.8 10.1 8.2 0 15.4-3.9 15.4-11.6 0-16-40.9-13.1-40.9-36.6 0-13.2 11.3-19.7 26-19.7 7.4 0 22.2 2.9 22.2 13.2v1.8c0 .5-.4.9-.9 1l-8.4.7zm55-16.8c20.1 0 30.6 15.5 30.6 33.6 0 1.7-.3 4.9-.3 4.9h-56c.3 18.4 13.2 29 28.3 29 10.9 0 18.6-6 21.1-8.2.5-.4 1.2-.3 1.5.2l3.7 5.7c.3.4.2 1-.2 1.3-2.5 2.2-12.3 9.9-26.5 9.9-21.6 0-38.2-15.7-38.2-38.2 0-23.8 16.4-38.2 36-38.2zm20.4 30.8c-.6-15.1-9.9-22.5-20.6-22.5-12.1 0-22.7 7.9-25.1 22.5h45.7z" fill="#53B6D0"></path>
+          <path d="M-644.8 507.5v-17.8c0-.6.4-1 1-1h7.6c6.8 0 9.8 3 9.8 9.7v25.8c0 3.6-.3 6.2-.3 6.2h.3c3.5-7.1 12.6-14.6 25.3-14.6 16.5 0 25.7 8.5 25.7 28v47.3h-8.6c-6.8 0-9.7-2.9-9.7-9.8v-33.7c0-9-2.5-15-11.7-15-9.7 0-17.2 6.4-19.9 15.3-.9 2.9-1.2 6.1-1.2 9.5v32.8c0 .6-.4 1-1 1h-16.3c-.6 0-1-.4-1-1v-82.7" fill="#FFF"></path>
+          <path d="M-368.8 530v-10.8c0-.6.4-1 1-1h.7c6.2 0 8.3 2.6 8.3 8.6V562c0 11.5 1.9 21.7 16 21.7 16.5 0 26.9-14.7 26.9-30.5v-33.9c0-.6.4-1 1-1h7.9c.6 0 1 .4 1 1v71.9h-.8c-6.1 0-8.9-2.6-8.9-8.2v-7.3h-.3c-2.9 7.1-12.2 17.2-28.1 17.2-17.3 0-24.5-9.3-24.5-28.3V530h-.2" fill="#53B6D0"></path>
+          <g fill="#53B6D0"><path d="M-489.3 532.3v58.4c0 .6.4 1 1 1h7.9c.6 0 1-.4 1-1v-33.4c0-3.4.3-6.7 1.2-9.7 3.1-11.1 11.9-21.4 23.8-21.4 13.8 0 15.4 10.7 15.4 21.7v43c0 .6.4 1 1 1h7.9c.6 0 1-.4 1-1v-33.6c0-4 .3-7.4 1.3-10.6 3-10.7 12.1-20.6 23.1-20.6 13.4 0 15.8 9.8 15.8 21.7v35.1c0 6.3-.3 8.9 5.9 8.9h3.1c.6 0 1-.4 1-1v-45.5c0-18-6.6-28.3-24.3-28.3-12.3 0-23 8.4-26.9 18.3h-.3c-2.2-12.1-9.2-18.3-22.7-18.3-11.3 0-22.8 8.5-26.4 18.4h-.3s.3-2.3.3-5.2v-3.3c0-5.6.2-8.2-5.9-8.2h-4l.1 13.6z"></path>
+            <path d="M-380.1 591.8c-6.2 0-8.9-2.6-8.9-8.9v-35.1c0-11.9-2.5-21.7-15.8-21.7-11 0-20.1 9.9-23.1 20.6-1 3.2-1.3 6.6-1.3 10.6v34.5h-9.9v-44c0-11-1.6-21.7-15.4-21.7-11.9 0-20.7 10.3-23.8 21.4-.9 3-1.2 6.4-1.2 9.7v34.5h-9.9v-61.2c0-2-1.2-3.2-3.2-3.2h-6.1v-8.6h10.2c6.1 0 8.9 2.6 8.9 8.2v3.3c0 2.9-.3 5.2-.3 5.2h.3c3.6-9.9 15.1-18.4 26.4-18.4 13.5 0 20.4 6.2 22.7 18.3h.3c3.9-9.9 14.5-18.3 26.9-18.3 17.7 0 24.3 10.3 24.3 28.3V580"></path>
+          </g>
         </svg>
         <span className="fs-36 muse-color jobs-text">Jobs</span>
       </div>
@@ -328,16 +322,16 @@ function App() {
         <div>
           <div>
             <form onChange={handleExperienceChange}>
-            <fieldset>
-              <legend>Experience</legend>
-              <input type="radio" id="entry" name="experience" value="Entry Level"/>
-              <label htmlFor="entry">Entry Level</label><br/>
-              <input type="radio" id="mid" name="experience" value="Mid Level"/>
-              <label htmlFor="mid">Mid Level</label><br/>
-              <input type="radio" id="senior" name="experience" value="Senior Level"/>
-              <label htmlFor="senior">Senior Level</label><br/>
-              <input type="radio" id="intern" name="experience" value="Internship"/>
-              <label htmlFor="intern">Internship</label><br/>
+              <fieldset>
+                <legend>Experience</legend>
+                <input type="radio" id="entry" name="experience" value="Entry Level" />
+                <label htmlFor="entry">Entry Level</label><br />
+                <input type="radio" id="mid" name="experience" value="Mid Level" />
+                <label htmlFor="mid">Mid Level</label><br />
+                <input type="radio" id="senior" name="experience" value="Senior Level" />
+                <label htmlFor="senior">Senior Level</label><br />
+                <input type="radio" id="intern" name="experience" value="Internship" />
+                <label htmlFor="intern">Internship</label><br />
               </fieldset>
             </form>
           </div>
@@ -353,33 +347,33 @@ function App() {
         <div>
           {
             jobs.length === 0 ? <p className="no-jobs">No jobs found</p> :
-          jobs?.map((job, index) => (
-            <article key={index} className="card">
-              <Job job={job} location={location} />
-            </article>
-          ))
-        }
+              jobs?.map((job, index) => (
+                <article key={index} className="card">
+                  <Job job={job} location={location} />
+                </article>
+              ))
+          }
         </div>
-     </div>
+      </div>
       <div className="buttons-container">
         <button className="chevron-button" onClick={handlePrevPage} disabled={page === 1}>
-        <span className="sr-only">Prev Page</span>
-          <svg className="chevron-left" style={{width:"24px",height:"24px", backgroundColor:"#f6f7fb"}} viewBox="0 0 24 24">
+          <span className="sr-only">Prev Page</span>
+          <svg className="chevron-left" style={{ width: "24px", height: "24px", backgroundColor: "#f6f7fb" }} viewBox="0 0 24 24">
             <path fill="gray" d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z" />
           </svg>
         </button>
-        <button className="number-button" onClick={()=> handlePageNumber(1)}>1</button>
-        <button className="number-button" onClick={()=> handlePageNumber(2)}>2</button>
-        <button className="number-button" onClick={()=> handlePageNumber(3)}>3</button>
+        <button className="number-button" onClick={() => handlePageNumber(1)}>1</button>
+        <button className="number-button" onClick={() => handlePageNumber(2)}>2</button>
+        <button className="number-button" onClick={() => handlePageNumber(3)}>3</button>
         <span className="flex">
-        <svg style={{width:"24px",height:"24px", backgroundColor:"#f6f7fb"}} viewBox="0 0 24 24">
-          <path fill="gray" d="M16,12A2,2 0 0,1 18,10A2,2 0 0,1 20,12A2,2 0 0,1 18,14A2,2 0 0,1 16,12M10,12A2,2 0 0,1 12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12M4,12A2,2 0 0,1 6,10A2,2 0 0,1 8,12A2,2 0 0,1 6,14A2,2 0 0,1 4,12Z" />
-        </svg>
+          <svg style={{ width: "24px", height: "24px", backgroundColor: "#f6f7fb" }} viewBox="0 0 24 24">
+            <path fill="gray" d="M16,12A2,2 0 0,1 18,10A2,2 0 0,1 20,12A2,2 0 0,1 18,14A2,2 0 0,1 16,12M10,12A2,2 0 0,1 12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12M4,12A2,2 0 0,1 6,10A2,2 0 0,1 8,12A2,2 0 0,1 6,14A2,2 0 0,1 4,12Z" />
+          </svg>
         </span>
-        <button className="number-button" onClick={()=> handlePageNumber(10)}>10</button>
+        <button className="number-button" onClick={() => handlePageNumber(10)}>10</button>
         <button className="chevron-button" onClick={handleNextPage} disabled={page === 1000}>
           <span className="sr-only">Next Page</span>
-          <svg style={{width:"24px",height:"24px", backgroundColor:"#f6f7fb"}} viewBox="0 0 24 24">
+          <svg style={{ width: "24px", height: "24px", backgroundColor: "#f6f7fb" }} viewBox="0 0 24 24">
             <path fill="gray" d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
           </svg>
         </button>
